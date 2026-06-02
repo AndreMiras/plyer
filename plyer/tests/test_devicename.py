AndreMiras/plyer

commit 91b2770b3f2573b5b90316b8ecb181b5b14414a8

Merge pull request #836 from GvozdevLeonid/master
Some plyer improvements

plyer/facades/orientation.py
@@ -40,6 +40,12 @@ class Orientation:
     Orientation facade.
     '''
 
+    def get_orientation(self) -> str:
+        '''
+        Return current screen rotation (landscape, landscape-reversed, portrait, portrait-reversed, unknown)
+        '''
+        return self._get_landscape()
+
     def set_landscape(self, reverse=False):
         '''
         Rotate the app to a landscape orientation.
@@ -70,6 +76,9 @@ class Orientation:
         self._set_sensor(mode=mode)
 
     # private
+
+    def _get_landscape(self, **kwargs):
+        raise NotImplementedError()
 
     def _set_landscape(self, **kwargs):
         raise NotImplementedError()

plyer/platforms/android/devicename.py
@@ -5,8 +5,6 @@ Module of Android API for plyer.devicename.
 from jnius import autoclass
 from plyer.facades import DeviceName
 
-Build = autoclass('android.os.Build')
-
 
 class AndroidDeviceName(DeviceName):
     '''
@@ -23,7 +21,18 @@ class AndroidDeviceName(DeviceName):
 
         Thereby making this method more backward compatible.
         """
-        return Build.MODEL
+
+        Build = autoclass('android.os.Build')
+        PythonActivity = autoclass('org.kivy.android.PythonActivity')
+        SettingsGlobal = autoclass('android.provider.Settings$Global')
+
+        context = PythonActivity.mActivity
+        name = SettingsGlobal.getString(context.getContentResolver(), SettingsGlobal.DEVICE_NAME)
+
+        if not name:
+            name = Build.MODEL
+
+        return name
 
 
 def instance():

plyer/platforms/android/keystore.py
@@ -1,10 +1,21 @@
+import base64
+import json
+
+from jnius import autoclass
+
 from plyer.facades import Keystore
 from plyer.platforms.android import activity
 
 
 class AndroidKeystore(Keystore):
-
     def _set_key(self, servicename, key, value, **kwargs):
+        cipher_text, iv = self.encrypt_value(value, servicename)
+
+        value = json.dumps({
+            'cipher': base64.b64encode(cipher_text).decode(),
+            'iv': base64.b64encode(iv).decode()
+        })
+
         mode = kwargs.get("mode", 0)
         settings = activity.getSharedPreferences(servicename, mode)
         editor = settings.edit()
@@ -14,12 +25,62 @@ class AndroidKeystore(Keystore):
     def _get_key(self, servicename, key, **kwargs):
         mode = kwargs.get("mode", 0)
         default = kwargs.get("default", "__None")
+
         settings = activity.getSharedPreferences(servicename, mode)
         ret = settings.getString(key, default)
-        if ret == "__None":
-            ret = None
-        return ret
+        if ret == default:
+            return None
 
+        blob = json.loads(ret)
+        return self.decrypt_value(servicename, blob)
+
+    def encrypt_value(self, value, servicename):
+        Cipher = autoclass('javax.crypto.Cipher')
+        secret_key = self.get_secret_key(servicename)
+
+        cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        cipher.init(Cipher.ENCRYPT_MODE, secret_key)
+
+        ciphertext = cipher.doFinal(value.encode("utf-8"))
+        iv = cipher.getIV()
+        return ciphertext, iv
+
+    def decrypt_value(self, servicename, data):
+        GCMParameterSpec = autoclass('javax.crypto.spec.GCMParameterSpec')
+        Cipher = autoclass('javax.crypto.Cipher')
+        String = autoclass('java.lang.String')
+
+        secret_key = self.get_secret_key(servicename)
+        cipher = Cipher.getInstance("AES/GCM/NoPadding")
+
+        iv = base64.b64decode(data['iv'])
+        ct = base64.b64decode(data['cipher'])
+
+        spec = GCMParameterSpec(128, iv)
+        cipher.init(Cipher.DECRYPT_MODE, secret_key, spec)
+        plaintext_bytes = cipher.doFinal(ct)
+        return str(String(plaintext_bytes, "UTF-8"))
+
+    @staticmethod
+    def get_secret_key(servicename):
+        KeyProperties = autoclass('android.security.keystore.KeyProperties')
+        KeyGenerator = autoclass('javax.crypto.KeyGenerator')
+        KeyGenParameterSpec = autoclass('android.security.keystore.KeyGenParameterSpec$Builder')
+        KeyStore = autoclass('java.security.KeyStore')
+
+        key_store = KeyStore.getInstance("AndroidKeyStore")
+        key_store.load(None)
+
+        if not key_store.containsAlias(servicename):
+            builder = KeyGenParameterSpec(servicename,
+                                        KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+            builder.setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+            builder.setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+            key_gen = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
+            key_gen.init(builder.build())
+            key_gen.generateKey()
+
+        return key_store.getKey(servicename, None)
 
 def instance():
     return AndroidKeystore()

plyer/platforms/android/orientation.py
@@ -3,6 +3,7 @@ from plyer.platforms.android import activity
 from plyer.facades import Orientation
 
 ActivityInfo = autoclass('android.content.pm.ActivityInfo')
+Surface = autoclass('android.view.Surface')
 
 
 class AndroidOrientation(Orientation):
@@ -37,6 +38,18 @@ class AndroidOrientation(Orientation):
         elif mode == 'portrait':
             activity.setRequestedOrientation(
                 ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT)
+
+    def _get_orientation(self):
+        orientations = {
+            Surface.ROTATION_0: 'portrait',
+            Surface.ROTATION_90: 'landscape',
+            Surface.ROTATION_180: 'portrait-reversed',
+            Surface.ROTATION_270: 'landscape-reversed',
+        }
+
+        rotation = activity.getWindowManager().getDefaultDisplay().getRotation()
+
+        return orientations.get(rotation, 'unknown')
 
 
 def instance():

plyer/platforms/linux/orientation.py
@@ -5,7 +5,7 @@ from plyer.facades import Orientation
 class LinuxOrientation(Orientation):
 
     def _set_landscape(self, **kwargs):
-        self.rotate = 'normal'
+        self.rotate = 'inverted' if kwargs['reverse'] else 'normal'
         self.screen = sb.check_output(
             "xrandr -q | grep ' connected' |  head -n 1 | cut -d ' ' -f1",
             shell=True
@@ -14,13 +14,36 @@ class LinuxOrientation(Orientation):
         sb.call(["xrandr", "--output", self.screen, "--rotate", self.rotate])
 
     def _set_portrait(self, **kwargs):
-        self.rotate = 'left'
+        self.rotate = 'right' if kwargs['reverse'] else 'left'
         self.screen = sb.check_output(
             "xrandr -q | grep ' connected' |  head -n 1 | cut -d ' ' -f1",
             shell=True
         )
         self.screen = self.screen.decode('utf-8').split('\n')[0]
         sb.call(["xrandr", "--output", self.screen, "--rotate", self.rotate])
+
+    def _get_orientation(self, **kwargs):
+        self.screen = sb.check_output(
+            "xrandr -q | grep ' connected' |  head -n 1 | cut -d ' ' -f1",
+            shell=True
+        )
+        self.screen = self.screen.decode('utf-8').split('\n')[0]
+
+        try:
+            orientation = sb.check_output(
+                f"xrandr -q --verbose | grep {self.screen} | sed 's/primary //' | awk '{{print $5}}'",
+                shell=True
+            ).decode('utf-8').strip()
+        except Exception:
+            return 'unknown'
+
+        orientations = {
+            'normal': 'landscape',
+            'inverted': 'landscape-reversed',
+            'left': 'portrait',
+            'right': 'portrait-reversed'
+        }
+        return orientations.get(orientation, 'unknown')
 
 
 def instance():

plyer/platforms/linux/wifi.py
@@ -115,6 +115,21 @@ class NMCLIWifi(Wifi):
 
         return connected
 
+    def _connected_ssid(self, interface=None):
+        if not self._is_enabled():
+            self._enable()
+
+        ssid = None
+        ssid_proc = Popen(['nmcli', '-t', '-f', 'ACTIVE,NAME', 'connection'], stdout=PIPE)
+        ssid_lines = ssid_proc.communicate()[0].decode('utf-8').splitlines()
+        for ssid_line in ssid_lines:
+            active, ssid_value = ssid_line.split(':')
+            if active == 'yes':
+                ssid = ssid_value
+                break
+
+        return ssid
+
     def _start_scanning(self, interface=None):
         '''
         Start scanning for available Wi-Fi networks
@@ -358,6 +373,21 @@ class LinuxWifi(Wifi):
                 connected = True
 
         return connected
+
+    def _connected_ssid(self, interface=None):
+        if not self._is_enabled():
+            self._enable()
+
+        ssid = None
+        ssid_proc = Popen(['nmcli', '-t', '-f', 'ACTIVE,NAME', 'connection'], stdout=PIPE)
+        ssid_lines = ssid_proc.communicate()[0].decode('utf-8').splitlines()
+        for ssid_line in ssid_lines:
+            active, ssid_value = ssid_line.split(':')
+            if active == 'yes':
+                ssid = ssid_value
+                break
+
+        return ssid
 
     def _start_scanning(self, interface=None):
         '''

plyer/platforms/macosx/gps.py
@@ -1,0 +1,117 @@
+import threading
+import time
+
+from pyobjus import autoclass, protocol
+from pyobjus.dylib_manager import INCLUDE, load_framework
+
+from plyer.facades import GPS
+
+load_framework(INCLUDE.CoreLocation)
+load_framework(INCLUDE.Foundation)
+
+
+CLLocationManager = autoclass('CLLocationManager')
+NSRunLoop = autoclass('NSRunLoop')
+NSObject = autoclass('NSObject')
+NSDate = autoclass('NSDate')
+
+
+class OSXGPS(GPS):
+    def __del__(self):
+        self._run_loop_thread_allow = False
+
+    def _configure(self):
+        if not hasattr(self, '_run_loop_thread'):
+            self._run_loop_thread_allow = True
+            self._is_running = False
+
+            self._run_loop_thread = threading.Thread(target=self._run_loop)
+            self._run_loop_thread.daemon = True
+            self._run_loop_thread.start()
+
+    def _run_loop(self):
+        self._location_manager = CLLocationManager.alloc().init()
+        self._location_manager.setDelegate_(self)
+        self._location_manager.desiredAccuracy = -1.0
+
+        self._location_manager.requestAlwaysAuthorization()
+        self._run_loop = NSRunLoop.currentRunLoop()
+
+        while self._run_loop_thread_allow:
+            if self._is_running:
+                next_date = NSDate.dateWithTimeIntervalSinceNow_(0.01)
+                self._run_loop.runMode_beforeDate_('NSDefaultRunLoopMode', next_date)
+            time.sleep(0.1)
+
+    def _start(self, **kwargs):
+        if not hasattr(self, '_location_manager'):
+            self.on_status('provider-disabled', 'standard-macos-provider: denied')
+            return
+
+        min_distance = kwargs.get('minDistance')
+        self._location_manager.distanceFilter = min_distance
+
+        if self._location_manager.authorizationStatus == 2:
+            if self.on_status:
+                self.on_status('provider-disabled', 'standard-macos-provider: denied')
+
+        self._location_manager.startUpdatingLocation()
+        self._is_running = True
+
+    def _stop(self):
+        if hasattr(self, '_location_manager'):
+            self._location_manager.stopUpdatingLocation()
+        self._is_running = False
+
+    @protocol('CLLocationManagerDelegate')
+    def locationManager_didChangeAuthorizationStatus_(self, manager, status):
+        if self.on_status:
+            s_status = ''
+            provider_status = ''
+            provider = 'standard-macos-provider'
+            if status == 0:
+                provider_status = 'provider-disabled'
+                s_status = 'notDetermined'
+            elif status == 1:
+                provider_status = 'provider-enabled'
+                s_status = 'restricted'
+            elif status == 2:
+                provider_status = 'provider-disabled'
+                s_status = 'denied'
+            elif status == 3:
+                provider_status = 'provider-enabled'
+                s_status = 'authorizedAlways'
+            elif status == 4:
+                provider_status = 'provider-enabled'
+                s_status = 'authorizedWhenInUse'
+
+            self.on_status(provider_status, f'{provider}: {s_status}')
+
+    @protocol('CLLocationManagerDelegate')
+    def locationManager_didUpdateLocations_(self, manager, locations):
+        location = manager.location
+
+        try:
+            description = location.description().UTF8String()
+        except Exception:
+            description = location.description.UTF8String()
+
+        split_description = description.split('<')[-1].split('>')[0].split(',')
+
+        lat, lon = [float(coord) for coord in split_description]
+        acc = float(description.split(' +/- ')[-1].split('m ')[0])
+
+        self.on_location(
+            **{
+                'lat': lat,
+                'lon': lon,
+                'altitude': location.altitude,
+                'speed': location.speed,
+                'course': location.course,
+                'acc': acc,
+            },
+        )
+
+
+def instance():
+    return OSXGPS()

plyer/platforms/macosx/wifi.py
@@ -23,6 +23,21 @@ class OSXWifi(Wifi):
         '''
         return CWWiFiClient.sharedWiFiClient().interface().powerOn()
 
+    def _is_connected(self, interface=None):
+        '''
+        Return whether a specified interface is connected to a WiFi network.
+        '''
+        if not self._is_enabled():
+            self._enable()
+
+        return CWInterface.interface().ssid() is not None
+
+    def _connected_ssid(self, interface=None):
+        if not self._is_enabled():
+            self._enable()
+
+        return CWInterface.interface().ssid().UTF8String()
+
     def _get_network_info(self, name):
         '''
         Returns all the network information.
@@ -83,7 +98,7 @@ class OSXWifi(Wifi):
                 'venueGroup': venueGroup,
                 'venueType': venueType}
 
-    def _start_scanning(self):
+    def _start_scanning(self, interface=None):
         '''
         Starts scanning for available Wi-Fi networks.
         '''
@@ -93,9 +108,13 @@ class OSXWifi(Wifi):
             scan = c.scanForNetworksWithName_error_(None, None)
             cnt = scan.allObjects().count()
             for i in range(cnt):
-                self.names[
-                    scan.allObjects().objectAtIndex_(i).ssid.UTF8String()
-                ] = scan.allObjects().objectAtIndex_(i)
+                if scan.allObjects().objectAtIndex_(i).ssid is not None:
+                    try:
+                        self.names[
+                            scan.allObjects().objectAtIndex_(i).ssid.UTF8String()
+                        ] = scan.allObjects().objectAtIndex_(i)
+                    except Exception:
+                        pass
         else:
             raise Exception("Wifi not enabled.")
 
@@ -105,7 +124,7 @@ class OSXWifi(Wifi):
         '''
         return self.names.keys()
 
-    def _connect(self, network, parameters):
+    def _connect(self, network, parameters, interface=None):
         '''
         Expects 2 parameters:
             - name/ssid of the network.
@@ -119,7 +138,7 @@ class OSXWifi(Wifi):
             None)
         return
 
-    def _disconnect(self):
+    def _disconnect(self, interface=None):
         '''
         Disconnect from network.
         '''

commit 4fadaec2867489e39bb0ba4589b33245c1bc165a

:green_heart: Fix CI failures
Resolve flake8 failures introduced in #836 by wrapping long lines, fixing
continuation indentation, restoring expected module spacing, and removing
unused global declarations.

Update the macOS devicename test to mock the new NSHost-based
implementation instead of socket.gethostname(), matching the code path
now used by the platform implementation.

plyer/facades/orientation.py
@@ -42,7 +42,7 @@ class Orientation:
 
     def get_orientation(self) -> str:
         '''
-        Return current screen rotation (landscape, landscape-reversed, portrait, portrait-reversed, unknown)
+        Return current screen rotation.
         '''
         return self._get_landscape()
 

plyer/platforms/android/devicename.py
@@ -27,7 +27,9 @@ class AndroidDeviceName(DeviceName):
         SettingsGlobal = autoclass('android.provider.Settings$Global')
 
         context = PythonActivity.mActivity
-        name = SettingsGlobal.getString(context.getContentResolver(), SettingsGlobal.DEVICE_NAME)
+        name = SettingsGlobal.getString(
+            context.getContentResolver(), SettingsGlobal.DEVICE_NAME
+        )
 
         if not name:
             name = Build.MODEL

plyer/platforms/android/keystore.py
@@ -65,22 +65,32 @@ class AndroidKeystore(Keystore):
     def get_secret_key(servicename):
         KeyProperties = autoclass('android.security.keystore.KeyProperties')
         KeyGenerator = autoclass('javax.crypto.KeyGenerator')
-        KeyGenParameterSpec = autoclass('android.security.keystore.KeyGenParameterSpec$Builder')
+        KeyGenParameterSpec = autoclass(
+            'android.security.keystore.KeyGenParameterSpec$Builder'
+        )
         KeyStore = autoclass('java.security.KeyStore')
 
         key_store = KeyStore.getInstance("AndroidKeyStore")
         key_store.load(None)
 
         if not key_store.containsAlias(servicename):
-            builder = KeyGenParameterSpec(servicename,
-                                        KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+            purpose = (
+                KeyProperties.PURPOSE_ENCRYPT |
+                KeyProperties.PURPOSE_DECRYPT
+            )
+            builder = KeyGenParameterSpec(servicename, purpose)
             builder.setBlockModes(KeyProperties.BLOCK_MODE_GCM)
-            builder.setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
-            key_gen = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
+            builder.setEncryptionPaddings(
+                KeyProperties.ENCRYPTION_PADDING_NONE
+            )
+            key_gen = KeyGenerator.getInstance(
+                KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore"
+            )
             key_gen.init(builder.build())
             key_gen.generateKey()
 
         return key_store.getKey(servicename, None)
+
 
 def instance():
     return AndroidKeystore()

plyer/platforms/android/orientation.py
@@ -47,7 +47,9 @@ class AndroidOrientation(Orientation):
             Surface.ROTATION_270: 'landscape-reversed',
         }
 
-        rotation = activity.getWindowManager().getDefaultDisplay().getRotation()
+        rotation = (
+            activity.getWindowManager().getDefaultDisplay().getRotation()
+        )
 
         return orientations.get(rotation, 'unknown')
 

plyer/platforms/linux/orientation.py
@@ -30,8 +30,12 @@ class LinuxOrientation(Orientation):
         self.screen = self.screen.decode('utf-8').split('\n')[0]
 
         try:
+            command = (
+                f"xrandr -q --verbose | grep {self.screen} | "
+                "sed 's/primary //' | awk '{print $5}'"
+            )
             orientation = sb.check_output(
-                f"xrandr -q --verbose | grep {self.screen} | sed 's/primary //' | awk '{{print $5}}'",
+                command,
                 shell=True
             ).decode('utf-8').strip()
         except Exception:

plyer/platforms/linux/wifi.py
@@ -120,7 +120,10 @@ class NMCLIWifi(Wifi):
             self._enable()
 
         ssid = None
-        ssid_proc = Popen(['nmcli', '-t', '-f', 'ACTIVE,NAME', 'connection'], stdout=PIPE)
+        ssid_proc = Popen(
+            ['nmcli', '-t', '-f', 'ACTIVE,NAME', 'connection'],
+            stdout=PIPE
+        )
         ssid_lines = ssid_proc.communicate()[0].decode('utf-8').splitlines()
         for ssid_line in ssid_lines:
             active, ssid_value = ssid_line.split(':')
@@ -379,7 +382,10 @@ class LinuxWifi(Wifi):
             self._enable()
 
         ssid = None
-        ssid_proc = Popen(['nmcli', '-t', '-f', 'ACTIVE,NAME', 'connection'], stdout=PIPE)
+        ssid_proc = Popen(
+            ['nmcli', '-t', '-f', 'ACTIVE,NAME', 'connection'],
+            stdout=PIPE
+        )
         ssid_lines = ssid_proc.communicate()[0].decode('utf-8').splitlines()
         for ssid_line in ssid_lines:
             active, ssid_value = ssid_line.split(':')

plyer/platforms/macosx/gps.py
@@ -40,12 +40,16 @@ class OSXGPS(GPS):
         while self._run_loop_thread_allow:
             if self._is_running:
                 next_date = NSDate.dateWithTimeIntervalSinceNow_(0.01)
-                self._run_loop.runMode_beforeDate_('NSDefaultRunLoopMode', next_date)
+                self._run_loop.runMode_beforeDate_(
+                    'NSDefaultRunLoopMode', next_date
+                )
             time.sleep(0.1)
 
     def _start(self, **kwargs):
         if not hasattr(self, '_location_manager'):
-            self.on_status('provider-disabled', 'standard-macos-provider: denied')
+            self.on_status(
+                'provider-disabled', 'standard-macos-provider: denied'
+            )
             return
 
         min_distance = kwargs.get('minDistance')
@@ -53,7 +57,9 @@ class OSXGPS(GPS):
 
         if self._location_manager.authorizationStatus == 2:
             if self.on_status:
-                self.on_status('provider-disabled', 'standard-macos-provider: denied')
+                self.on_status(
+                    'provider-disabled', 'standard-macos-provider: denied'
+                )
 
         self._location_manager.startUpdatingLocation()
         self._is_running = True

plyer/platforms/macosx/wifi.py
@@ -110,9 +110,10 @@ class OSXWifi(Wifi):
             for i in range(cnt):
                 if scan.allObjects().objectAtIndex_(i).ssid is not None:
                     try:
+                        network = scan.allObjects().objectAtIndex_(i)
                         self.names[
-                            scan.allObjects().objectAtIndex_(i).ssid.UTF8String()
-                        ] = scan.allObjects().objectAtIndex_(i)
+                            network.ssid.UTF8String()
+                        ] = network
                     except Exception:
                         pass
         else:

plyer/platforms/win/libs/wifi_defs.py
@@ -290,7 +290,6 @@ def _connect(network, parameters):
     '''
     Attempts to connect to a specific network.
     '''
-    global _dict
     wireless_interface = _dict[network]
 
     wcp = WLAN_CONNECTION_PARAMETERS()
@@ -480,9 +479,6 @@ def _get_network_info(name):
     '''
     returns the list of the network selected in a dict.
     '''
-    global available
-    global _dict
-
     net = _dict[name]
     dot11BssType = net.dot11BssType
     dot11DefaultAuthAlgorithm = net.dot11DefaultAuthAlgorithm
@@ -505,7 +501,6 @@ def _make_dict():
     '''
     Prepares a dict so it could store network information.
     '''
-    global available
     global _dict
     _dict = {}
     for network in available:
@@ -516,7 +511,6 @@ def _get_available_wifi():
     '''
     returns the available wifi networks.
     '''
-    global _dict
     return _dict
 
 

plyer/tests/test_devicename.py
@@ -65,9 +65,22 @@ class TestDeviceName(unittest.TestCase):
                                      )
         devicename_instance = devicename.instance()
 
-        with patch.object(socket,
-                          'gethostname',
-                          return_value='mocked_macosx_hostname'
+        class LocalizedName:
+            @staticmethod
+            def UTF8String():
+                return 'mocked_macosx_hostname'
+
+        class CurrentHost:
+            localizedName = LocalizedName()
+
+        class NSHost:
+            @staticmethod
+            def currentHost():
+                return CurrentHost()
+
+        with patch.object(devicename,
+                          'autoclass',
+                          return_value=NSHost
                           ) as _:
 
             evaluated_device_name = devicename_instance.device_name